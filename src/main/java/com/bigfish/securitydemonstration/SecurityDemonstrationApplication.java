6b9ddb7748f5c76cfb90e9d7ef03fbe8b0a97f60
Geza Czimeth <gezaczimeth@gmail.com>
2023-02-24 08:27:18 +0100

    Package structure have been refactored

--- src/main/java/com/bigfish/securitydemonstration/SecurityDemonstrationApplication.java
+++ src/main/java/com/bigfish/securitydemonstration/SecurityDemonstrationApplication.java
@@ -1,4 +1,4 @@
-package com.bigfish.securitydemonstrationgezaczimeth;
+package com.bigfish.securitydemonstration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
