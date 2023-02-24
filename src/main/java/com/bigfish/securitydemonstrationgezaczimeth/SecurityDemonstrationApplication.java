b461fd6c060f4a52b33bdb1a9bdc642c0db5a689
Geza Czimeth <gezaczimeth@gmail.com>
2023-02-24 08:24:22 +0100

    Project Init

---
+++ src/main/java/com/bigfish/securitydemonstrationgezaczimeth/SecurityDemonstrationApplication.java
@@ -0,0 +1,13 @@
+package com.bigfish.securitydemonstrationgezaczimeth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityDemonstrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityDemonstrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bigfish/securitydemonstrationgezaczimeth/SecurityDemonstrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigfish.securitydemonstrationgezaczimeth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityDemonstrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
